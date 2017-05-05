7d22b32530d4862d2b756466aed10e9e1254d657
Waqar Hocain <waqarmaxtan@gmail.com>
2017-05-04 22:09:05 -0400

    Implement strict-mode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // DOM elements
 var state_container = $(".state"),
+	strict_game_mode = $(".mode input"),
 	buttons = $(".board button"),
 	btn_play = $(".play"),
 	btn_reset = $(".reset");
@@ -9,12 +10,13 @@ var state_container = $(".state"),
 var round = 1,
 	board_is_locked = true,
 	sequence = [],
+	strict_mode_enabled = false,
 	current_sequence = [];
 
 
 // attach event listeners
 btn_play.on("click", start_new_game);
-btn_reset.on("click", restart_game);
+btn_reset.on("click", reset_game);
 
 for (var i = 0; i < buttons.length; i++) {
 	$(buttons[i]).on("click", user_click_handler);
@@ -30,9 +32,9 @@ function generate_random_num(max) {
 
 function generate_play_sequence() {
 	// !!! this func has side effects :( it is modifying `sequence` global variable !!!
-	// fills sequence array with 23 random numbers for a new game
-	// (if you made it upto 23, you're a winner;)\)
-	for (var i = 0; i < 23; i++) {
+	// fills sequence array with 20 random numbers for a new game
+	// (if you made it upto 20, you're a winner;)\)
+	for (var i = 0; i < 20; i++) {
 		var random_num = generate_random_num(buttons.length);
 		sequence.push(random_num);
 	}
@@ -41,6 +43,7 @@ function generate_play_sequence() {
 
 function play_sequence() {
 	var i = 0;
+
 	board_is_locked = true;
 	current_sequence = sequence.slice(0, round);
 
@@ -76,10 +79,15 @@ function play_audio(elem) {
 
 
 function start_new_game() {
+	// clear previous games data
 	reset_game_data();
 
-	// disable the play button
+	if (strict_game_mode.is(":checked")) {
+		strict_mode_enabled = true;
+	}	
+
 	btn_play.prop("disabled", true);
+	strict_game_mode.prop("disabled", true);
 
 	generate_play_sequence();
 	start_new_round();
@@ -91,14 +99,15 @@ function start_new_round() {
 	play_sequence();
 }
 
-function restart_game() {
+function reset_game() {
 	reset_game_data();
 
-	// enable the play button
+	// enable the play button and game_mode checkbox
 	btn_play.prop("disabled", false);
+	strict_game_mode.prop("disabled", false);
 
 	render_state();
-	window.clearTimeout();		
+	window.clearTimeout();	
 }
 
 
@@ -127,16 +136,21 @@ function user_click_handler(e) {
 	play_audio(user_move);
 
 	if (user_move == desired_move) {
-		// if user has already clicked the generated sequence
 		if (current_sequence.length == 0) {
 			round++;
-			setTimeout(start_new_round, 800);
+			window.setTimeout(start_new_round, 800);
 			return null;
 		}
 	}
 	else {
-		state_container.html("<strong>YOU MADE A MISTAKE!!!</strong>");
-		setTimeout(play_sequence, 800)
+		state_container.html("YOU MADE A MISTAKE!!!");
+
+		if (strict_mode_enabled) {
+			window.setTimeout(start_new_game, 800);
+			return;
+		}
+
+		window.setTimeout(play_sequence, 800);
 	}
 
 }
